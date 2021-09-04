9e0b4dae14d9daa88ace464049c87d44200f9ced
Nam Hoang (particle4dev) <particle4dev@gmail.com>
2021-09-04 11:19:09 +0700

    add material ui lib

---
+++ components/ThemeProvider.tsx
@@ -0,0 +1,65 @@
+import * as React from 'react';
+import mediaQuery from 'css-mediaquery';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import { ThemeProvider } from '@material-ui/styles';
+import { Theme as ThemeType } from '@material-ui/core/styles';
+
+const isClient = process.browser;
+
+// https://material-ui.com/customization/breakpoints/#theme-breakpoints-up-key-media-query
+// https://stackoverflow.com/questions/13315131/enforcing-the-type-of-the-indexed-members-of-a-typescript-object
+interface IStringTMap<T> { [key: string]: T }
+type IStringNumberMap = IStringTMap<number>
+
+const width: IStringNumberMap = {
+  mobile: 479,
+  tablet: 767,
+  desktop: 1024,
+  smarttv: 1920,
+  // console, not supported
+  // wearable, not supported
+  // embedded not supported
+};
+
+export type ThemeProps = {
+  children: React.ReactNode;
+  deviceType?: string;
+  theme: ThemeType;
+}
+
+function Theme(props: ThemeProps) {
+  const { children, deviceType, theme } = props;
+
+  if(!isClient) {
+    const ssrMatchMedia = (query: string): any => ({
+      matches: mediaQuery.match(query, {
+        // The estimated CSS width of the browser.
+        width: deviceType ? width[deviceType] : 0 // default value is 0
+      }),
+      addListener: null,
+      removeListener: null
+    });
+
+    theme.props = {
+      // Change the default options of useMediaQuery
+      MuiUseMediaQuery: { ssrMatchMedia },
+    };
+  }
+
+  return (
+    <ThemeProvider theme={theme}>
+      {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+      <CssBaseline />
+      { children }
+    </ThemeProvider>
+  );
+}
+
+if (process.env.NODE_ENV !== 'production') {
+  Theme.displayName = 'components__ThemeProvider';
+}
+
+Theme.defaultProps = {};
+
+
+export default Theme;
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,24 @@
 import '../styles/globals.css'
+import NextHead from 'next/head'
 import type { AppProps } from 'next/app'
+import RobotoHead from '../components/RobotoHead'
+import ThemeProvider from '../components/ThemeProvider'
+import GlobalStyle from '../components/GlobalStyle'
+import Global from '../components/Global'
+import light from '../components/lighttheme'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <>
+    <NextHead>
+      <meta charSet="utf-8" />
+    </NextHead>
+    <ThemeProvider theme={light}>
+      <Component {...pageProps} />
+      <GlobalStyle />
+      <Global />
+    </ThemeProvider>
+    <RobotoHead />
+  </>
 }
+
 export default MyApp
